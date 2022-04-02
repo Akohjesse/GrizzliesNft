d292334642034b3d644b088cf540b9d4e739b717
Jesse <akohgodwinjesse@gmail.com>
2022-04-02 11:00:15 +0100

    bad vibes

---
+++ src/components/heroSection.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="temp_wrap">
+        <header class="flex flex_r">
+            <div class="logo">
+                <img src="@/assets/logo.svg" alt="" />
+            </div>
+            <h3>@GrizzliesNFT</h3>
+        </header>
+
+        <div class="hero_content">
+            <div class="lft_flg">
+                <div class="txt">
+                    <h1>Buy & Sell NFTs On The Web</h1>
+                    <p>
+                        NFTs will continue to revolutionize the way that artists
+                        and fans create community together.
+                    </p>
+                </div>
+                <div class="cta">
+                    <div class="btn_mono">
+                        <button>How It Works</button>
+                    </div>
+                    <a href="">Learn More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
