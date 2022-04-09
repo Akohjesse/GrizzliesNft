c335c00090616536551d220b40daf2eaf2bae10f
Jesse <akohgodwinjesse@gmail.com>
2022-04-09 21:11:51 +0100

    aje

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 import hero from '@/components/heroSection.vue'
+import Nav from '@/components/navbar.vue'
 </script>
 
 <template>
     <hero />
+    <Nav />
 </template>
 
-<style lang="scss">
-@import './scss/_generic.scss';
-</style>
+<style lang="scss" src="@/scss/generic.scss"></style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="wrap">
+        <nav class="flex flex_r">
+            <p>Index</p>
+            <p>Featured</p>
+            <p>Live Auctions</p>
+            <p>Collections</p>
+            <p>Creators</p>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+    padding: 4rem 0;
+    nav {
+        margin: 0 auto;
+        width: 35%;
+        border: 1px solid white;
+        padding: 7px 15px;
+        border-radius: 10px;
+    }
+}
+</style>
