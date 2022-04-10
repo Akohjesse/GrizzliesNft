a82d758046f7b12c0153ee3f0fd30e46d899a56b
Jesse <akohgodwinjesse@gmail.com>
2022-04-10 13:18:07 +0100

    gevrtvgdgb

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import hero from '@/components/heroSection.vue'
+import carousel from '@/components/carousel.vue'
 import Nav from '@/components/navbar.vue'
 </script>
 
@@ -13,6 +14,7 @@ import Nav from '@/components/navbar.vue'
     <div class="bg_right"></div>
     <hero />
     <Nav />
+    <carousel />
 </template>
 
 <style lang="scss" src="@/scss/generic.scss"></style>
--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -1,27 +1,32 @@
 <template>
-  <div class="divette flex">
-      <div class="car_wrap">
-          <div class="img">
-              <img src="" alt="">
-          </div>
-          <div class="name">
-              <small>Item Name</small>
-              <p>Robotic Bug</p>
-          </div>
-          <div class="bid">
-              <small>Current Bid</small>
-              <img src="@/assets/" alt=""> <p>0.005 ETH</p>
-          </div>
-      </div>
-  </div>
+    <div class="divette flex">
+        <div class="car_wrap flex_col flex">
+            <div class="img">
+                <img src="@/assets/ft1.svg" alt="" />
+            </div>
+            <div class="flex ctnt flex_r">
+                <div class="name">
+                    <small>Item Name</small>
+                    <p>Robotic Bug</p>
+                </div>
+                <div class="bid">
+                    <small>Current Bid</small>
+                   <div class="flex_row flex">
+                        <img src="@/assets/bid1.svg" alt="" />
+                        &nbsp; &nbsp;
+                        <p>0.005 ETH</p>
+                   </div>  
+                </div>
+            </div>
+            <div class="btn_mono btn_long">
+                <button>Buy Now</button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
-</style>
+<style lang="scss" src="@/scss/carousel.scss"></style>
