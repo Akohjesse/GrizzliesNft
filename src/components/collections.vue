d239fa60b2d48e934927478dd43fb0b4c61a04bc
Jesse <akohgodwinjesse@gmail.com>
2022-04-10 21:00:23 +0100

    collections complete

--- src/components/collections.vue
+++ src/components/collections.vue
@@ -11,12 +11,14 @@ import db from '@/db/collections.json'
         <br />
         <div class="grid container">
             <div v-for="item in db" :key="item.id">
-                <h1 class="abs"> <img src="@/assets/pref.svg" alt=""> {{item.name}}</h1>
-                <div class="seller_id flex">
-                        <img src="@/assets/bid1.svg" alt="" />
-                        &nbsp; &nbsp;
-                        <p class="name">@Johny</p>
-                    </div>
+                <h1 class="abs">
+                    <img src="@/assets/pref.svg" alt="" /> {{ item.name }}
+                </h1>
+                <div class="seller_id abs flex flex_r">
+                    <img src="@/assets/bid1.svg" alt="" />
+                    &nbsp; &nbsp;
+                    <p class="name">@Johny</p>
+                </div>
                 <img :src="require(`@/assets/0${item.id}.svg`)" alt="" />
                 <div class="top abs">
                     <img :src="require(`@/assets/l${item.id}.svg`)" alt="" />
