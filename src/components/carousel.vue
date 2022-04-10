0589b874899a6555ae1755981bccccb87aec174a
Jesse <akohgodwinjesse@gmail.com>
2022-04-10 12:11:11 +0100

    dududbdmom

---
+++ src/components/carousel.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="divette flex">
+      <div class="car_wrap">
+          <div class="img">
+              <img src="" alt="">
+          </div>
+          <div class="name">
+              <small>Item Name</small>
+              <p>Robotic Bug</p>
+          </div>
+          <div class="bid">
+              <small>Current Bid</small>
+              <img src="@/assets/" alt=""> <p>0.005 ETH</p>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
