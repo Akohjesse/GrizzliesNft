354be87049606648860a42bed5f9e809752cf666
Jesse <akohgodwinjesse@gmail.com>
2022-04-10 18:41:54 +0100

    aone 2

--- src/components/auctions.vue
+++ src/components/auctions.vue
@@ -4,25 +4,41 @@
             <p>Live Auctions</p>
             <small>View all</small>
         </div>
+        <br>
         <div class="flex flex_r box">
             <div class="metadata flex flex_col">
-                <p>Robotic Bug</p>
-                <div class="name">
-                    <small>Collection Name</small>
-                    <p>N33-014</p>
-                </div>
-                <div class="ctnt flex flex_r">
-                    <div class="name">
-                        <small>Current Bid</small>
-                        <p>0.005 ETH</p>
+                <br />
+                <div class="txt flex flex_col">
+                    <p class="name">Robotic Bug</p>
+                    <div>
+                        <small>Collection Name</small>
+                        <p>N33-014</p>
                     </div>
-                    <div class="name">
-                        <small>Bidding ends in</small>
-                        <p>2 minutes</p>
+                    <div class="ctnt flex flex_r">
+                        <div>
+                            <small>Current Bid</small>
+                            <p>0.005 ETH</p>
+                        </div>
+                        <div>
+                            <small>Bidding ends in</small>
+                            <p>2 minutes</p>
+                        </div>
                     </div>
                 </div>
                 <div class="act_sect">
                     <small>Seller</small>
+                    <div class="seller_id flex">
+                        <img src="@/assets/bid1.svg" alt="" />
+                        &nbsp; &nbsp;
+                        <p class="name">@Johny</p>
+                    </div>
+                    <div class="arrows">
+                        <div class="left"></div>
+                        <div class="right"></div>
+                    </div>
+                </div>
+                <div class="btn_mono btn_long">
+                    <button>Place a bid</button>
                 </div>
             </div>
             <div class="img">
