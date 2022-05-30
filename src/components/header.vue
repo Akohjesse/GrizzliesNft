c9614907b19ad3347a61f1e2e27cf638fb91c66c
Jesse <akohgodwinjesse@gmail.com>
2022-05-30 21:30:18 +0100

    Update header.vue

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,10 +1,10 @@
 <template>
-    <header id="top" class="flex flex_r temp_wrap">
-        <div class="logo">
-            <img src="@/assets/logo.svg" alt="" />
-        </div>
-        <h3>@GrizzliesNFT</h3>
-    </header>
+     <header id="top" class="flex flex_r temp_wrap">
+          <div class="logo">
+               <img src="@/assets/logo.svg" alt="" />
+          </div>
+          <h3>@GrizzliesNFT</h3>
+     </header>
 </template>
 
 <script>
@@ -13,13 +13,13 @@ export default {}
 
 <style lang="scss" scoped>
 header {
-    padding: 2.5rem 0px;
-    cursor: pointer;
-    h3 {
-        padding: 15px 2rem;
-        border-radius: 1rem;
-        background: #131314;
-        font-size: 1.3em;
-    }
+     padding: 2.5rem 0px;
+     cursor: pointer;
+     h3 {
+          padding: 15px 2rem;
+          border-radius: 1rem;
+          background: #131314;
+          font-size: 1.3em;
+     }
 }
 </style>
