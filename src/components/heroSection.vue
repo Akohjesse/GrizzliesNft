639f8cc6c0c3682be880bbbfb5990d30a7d24efe
Jesse <akohgodwinjesse@gmail.com>
2022-05-30 21:29:51 +0100

    Update heroSection.vue

--- src/components/heroSection.vue
+++ src/components/heroSection.vue
@@ -3,65 +3,65 @@ import { onMounted } from '@vue/runtime-core'
 import animateSection from '../animations/stagger_img'
 
 onMounted(() => {
-    animateSection()
+     animateSection()
 })
 </script>
 
 <template>
-    <div class="temp_wrap">
-        <div class="hero_content flex flex_r">
-            <div class="txt flex_col flex">
-                <h1>Buy & Sell NFTs On The Web</h1>
-                <p>
-                    NFTs will continue to revolutionize the way that artists and
-                    fans create community together.
-                </p>
-                <div class="cta flex">
-                    <div class="btn_mono">
-                        <button>How It Works</button>
-                    </div>
+     <div class="temp_wrap">
+          <div class="hero_content flex flex_r">
+               <div class="txt flex_col flex">
+                    <h1>Buy & Sell NFTs On The Web</h1>
                     <p>
-                        <a href="">Learn More</a>
+                         NFTs will continue to revolutionize the way that
+                         artists and fans create community together.
                     </p>
-                </div>
-            </div>
-            <div class="right_flg flex flex_r">
-                <div class="img flex_col">
-                    <div class="indiv">
-                        <img src="@/assets/ast01.svg" alt="" />
+                    <div class="cta flex">
+                         <div class="btn_mono">
+                              <button>How It Works</button>
+                         </div>
+                         <p>
+                              <a href="">Learn More</a>
+                         </p>
                     </div>
-                    <div class="indiv">
-                        <img src="@/assets/ast02.svg" alt="" />
+               </div>
+               <div class="right_flg flex flex_r">
+                    <div class="img flex_col">
+                         <div class="indiv">
+                              <img src="@/assets/ast01.svg" alt="" />
+                         </div>
+                         <div class="indiv">
+                              <img src="@/assets/ast02.svg" alt="" />
+                         </div>
+                         <div class="indiv">
+                              <img src="@/assets/ast03.svg" alt="" />
+                         </div>
                     </div>
-                    <div class="indiv">
-                        <img src="@/assets/ast03.svg" alt="" />
+                    <div class="img flex_col">
+                         <div class="indiv">
+                              <img src="@/assets/ast04.svg" alt="" />
+                         </div>
+                         <div class="indiv">
+                              <img src="@/assets/ast05.svg" alt="" />
+                         </div>
+                         <div class="indiv">
+                              <img src="@/assets/ast06.svg" alt="" />
+                         </div>
                     </div>
-                </div>
-                <div class="img flex_col">
-                    <div class="indiv">
-                        <img src="@/assets/ast04.svg" alt="" />
+                    <div class="img flex_col">
+                         <div class="indiv">
+                              <img src="@/assets/ast07.svg" alt="" />
+                         </div>
+                         <div class="indiv">
+                              <img src="@/assets/ast08.svg" alt="" />
+                         </div>
+                         <div class="indiv">
+                              <img src="@/assets/ast09.svg" alt="" />
+                         </div>
                     </div>
-                    <div class="indiv">
-                        <img src="@/assets/ast05.svg" alt="" />
-                    </div>
-                    <div class="indiv">
-                        <img src="@/assets/ast06.svg" alt="" />
-                    </div>
-                </div>
-                <div class="img flex_col">
-                    <div class="indiv">
-                        <img src="@/assets/ast07.svg" alt="" />
-                    </div>
-                    <div class="indiv">
-                        <img src="@/assets/ast08.svg" alt="" />
-                    </div>
-                    <div class="indiv">
-                        <img src="@/assets/ast09.svg" alt="" />
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+               </div>
+          </div>
+     </div>
 </template>
 
 <style lang="scss" src="@/scss/hero.scss"></style>
