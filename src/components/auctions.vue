53b366ca5fc153a501fe1fb36a375264c107e57c
Jesse <akohgodwinjesse@gmail.com>
2022-05-30 21:30:26 +0100

    Update auctions.vue

--- src/components/auctions.vue
+++ src/components/auctions.vue
@@ -3,83 +3,83 @@ import { ref } from '@vue/reactivity'
 
 const int = ref(1)
 const forward = () => {
-    if (int.value > 2) {
-        int.value = 1
-    } else {
-        int.value++
-    }
+     if (int.value > 2) {
+          int.value = 1
+     } else {
+          int.value++
+     }
 }
 const backward = () => {
-    if (int.value < 2) {
-        int.value = 3
-    } else {
-        int.value--
-    }
+     if (int.value < 2) {
+          int.value = 3
+     } else {
+          int.value--
+     }
 }
 </script>
 
 <template>
-    <div class="temp_wrap">
-        <div class="flex header flex_r">
-            <p>Live Auctions</p>
-            <small>View all</small>
-        </div>
-        <br />
-        <div class="flex flex_r box">
-            <div class="metadata flex flex_col">
-                <br />
-                <div class="txt flex flex_col">
-                    <p class="name">Robotic Bug</p>
-                    <div>
-                        <small>Collection Name</small>
-                        <p>N33-014</p>
+     <div class="temp_wrap">
+          <div class="flex header flex_r">
+               <p>Live Auctions</p>
+               <small>View all</small>
+          </div>
+          <br />
+          <div class="flex flex_r box">
+               <div class="metadata flex flex_col">
+                    <br />
+                    <div class="txt flex flex_col">
+                         <p class="name">Robotic Bug</p>
+                         <div>
+                              <small>Collection Name</small>
+                              <p>N33-014</p>
+                         </div>
+                         <div class="ctnt flex flex_r">
+                              <div>
+                                   <small>Current Bid</small>
+                                   <p>0.005 ETH</p>
+                              </div>
+                              <div>
+                                   <small>Bidding ends in</small>
+                                   <p>2 minutes</p>
+                              </div>
+                         </div>
                     </div>
-                    <div class="ctnt flex flex_r">
-                        <div>
-                            <small>Current Bid</small>
-                            <p>0.005 ETH</p>
-                        </div>
-                        <div>
-                            <small>Bidding ends in</small>
-                            <p>2 minutes</p>
-                        </div>
+                    <div class="act_sect">
+                         <small>Seller</small>
+                         <div class="flex flex_r">
+                              <div class="seller_id flex">
+                                   <img src="@/assets/bid1.svg" alt="" />
+                                   &nbsp; &nbsp;
+                                   <p class="name">@Johny</p>
+                              </div>
+                              <div class="arrows flex flex_r">
+                                   <img
+                                        src="@/assets/arr1.svg"
+                                        @click="backward"
+                                        alt=""
+                                   />
+                                   <img
+                                        src="@/assets/arr2.svg"
+                                        @click="forward"
+                                        alt=""
+                                   />
+                              </div>
+                         </div>
                     </div>
-                </div>
-                <div class="act_sect">
-                    <small>Seller</small>
-                    <div class="flex flex_r">
-                        <div class="seller_id flex">
-                            <img src="@/assets/bid1.svg" alt="" />
-                            &nbsp; &nbsp;
-                            <p class="name">@Johny</p>
-                        </div>
-                        <div class="arrows flex flex_r">
-                            <img
-                                src="@/assets/arr1.svg"
-                                @click="backward"
-                                alt=""
-                            />
-                            <img
-                                src="@/assets/arr2.svg"
-                                @click="forward"
-                                alt=""
-                            />
-                        </div>
+                    <div class="btn_mono btn_long">
+                         <button>Place a bid</button>
                     </div>
-                </div>
-                <div class="btn_mono btn_long">
-                    <button>Place a bid</button>
-                </div>
-            </div>
-            <div class="img">
-                <img
-                    ref="image"
-                    :src="require(`@/assets/ac${int}.svg`)"
-                    alt=""
-                />
-            </div>
-        </div>
-    </div>
+               </div>
+               <div class="img">
+                    <img
+                         ref="image"
+                         :src="require(`@/assets/ac${int}.svg`)"
+                         alt=""
+                    />
+               </div>
+          </div>
+     </div>
 </template>
 
 <style lang="scss" src="@/scss/auctions.scss" scoped></style>
