01fd176557a4d79b2975845841103d22e7d3bed3
Jesse <akohgodwinjesse@gmail.com>
2022-05-30 21:30:28 +0100

    Update stagger_img.js

--- src/animations/stagger_img.js
+++ src/animations/stagger_img.js
@@ -1,12 +1,12 @@
 import { gsap, Circ } from 'gsap'
 
 const animateSection = () => {
-    gsap.from('.img .indiv', {
-        opacity: 0,
-        ease: Circ.easeInOut,
-        stagger: 0.5,
-        duration: 1,
-    })
+     gsap.from('.img .indiv', {
+          opacity: 0,
+          ease: Circ.easeInOut,
+          stagger: 0.5,
+          duration: 1,
+     })
 }
 
 export default animateSection
