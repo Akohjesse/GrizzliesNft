e8f26da8ec58dc0e027c25ecc42e8cb9fcdb9264
Jesse <akohgodwinjesse@gmail.com>
2022-05-30 21:29:48 +0100

    Update navbar.vue

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="wrap">
-        <nav class="flex flex_r">
-            <p>Index</p>
-            <p>Featured</p>
-            <p>Live Auctions</p>
-            <p>Collections</p>
-            <p>Creators</p>
-        </nav>
-    </div>
+     <div class="wrap">
+          <nav class="flex flex_r">
+               <p>Index</p>
+               <p>Featured</p>
+               <p>Live Auctions</p>
+               <p>Collections</p>
+               <p>Creators</p>
+          </nav>
+     </div>
 </template>
 
 <script>
@@ -16,24 +16,24 @@ export default {}
 
 <style lang="scss" scoped>
 .wrap {
-    padding: 6rem 0;
-    nav {
-        margin: 0 auto;
-        width: 40%;
-        border: 1px solid white;
-        padding: 10px 2rem;
-        border-radius: 10px;
-        @media screen and (max-width: 1200px) {
-            width: 60%;
-        }
-        p {
-            &:nth-child(1) {
-                background: white;
-                color: black;
-                border-radius: 5%;
-                padding: 9px 15px;
-            }
-        }
-    }
+     padding: 6rem 0;
+     nav {
+          margin: 0 auto;
+          width: 40%;
+          border: 1px solid white;
+          padding: 10px 2rem;
+          border-radius: 10px;
+          @media screen and (max-width: 1200px) {
+               width: 60%;
+          }
+          p {
+               &:nth-child(1) {
+                    background: white;
+                    color: black;
+                    border-radius: 5%;
+                    padding: 9px 15px;
+               }
+          }
+     }
 }
 </style>
