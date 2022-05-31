2862d6b60d287f4d102b3b001acc698f090deb7a
Jesse <akohgodwinjesse@gmail.com>
2022-05-31 02:37:55 +0100

    Update carousel.vue

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -6,19 +6,23 @@ import ScrollTrigger from 'gsap/ScrollTrigger'
 onMounted(() => {
      let sections = gsap.utils.toArray('.carousel_container .car_wrap')
      gsap.registerPlugin(ScrollTrigger)
-     gsap.timeline({
-          scrollTrigger: {
-               trigger: '.carousel',
-               scrub: 3,
-               start: 'center center',
-               end: 'bottom +=300',
-               pin: '.carousel',
-               toggleActions: 'play pause resume restart',
+     ScrollTrigger.matchMedia({
+          '(min-width:600px)': function () {
+               gsap.timeline({
+                    scrollTrigger: {
+                         trigger: '.carousel',
+                         scrub: 3,
+                         start: 'center center',
+                         end: 'bottom +=300',
+                         pin: '.carousel',
+                         toggleActions: 'play pause resume restart',
+                    },
+               }).to('.carousel', {
+                    xPercent: 100 * (sections.length - 2),
+                    duration: 20,
+                    ease: Circ.easeInOut,
+               })
           },
-     }).to('.carousel', {
-          xPercent: 100 * (sections.length - 2),
-          duration: 20,
-          ease: Circ.easeInOut,
      })
 })
 </script>
