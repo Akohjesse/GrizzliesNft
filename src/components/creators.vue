bb5cdb622144f48c5f0c51c04c732696e4f4f4fd
Jesse <akohgodwinjesse@gmail.com>
2022-05-31 02:43:14 +0100

    wen merge

--- src/components/creators.vue
+++ src/components/creators.vue
@@ -7,19 +7,23 @@ import ScrollTrigger from 'gsap/ScrollTrigger'
 onMounted(() => {
      let sections = gsap.utils.toArray('.creator_wrap .car_wrap')
      gsap.registerPlugin(ScrollTrigger)
-     gsap.timeline({
-          scrollTrigger: {
-               trigger: '.creators',
-               scrub: 2,
-               start: 'center center',
-               end: 'bottom +=300',
-               pin: '.creators',
-               toggleActions: 'play reset none restart',
+     ScrollTrigger.matchMedia({
+          '(min-width:700px)': function () {
+               gsap.timeline({
+                    scrollTrigger: {
+                         trigger: '.creators',
+                         scrub: 2,
+                         start: 'center center',
+                         end: 'bottom +=300',
+                         pin: '.creators',
+                         toggleActions: 'play reset none restart',
+                    },
+               }).to('.creators .creator_wrap', {
+                    xPercent: -70 * (sections.length - 2),
+                    duration: 20,
+                    ease: Circ.easeInOut,
+               })
           },
-     }).to('.creators .creator_wrap', {
-          xPercent: -70 * (sections.length - 2),
-          duration: 20,
-          ease: Circ.easeInOut,
      })
 })
 </script>
